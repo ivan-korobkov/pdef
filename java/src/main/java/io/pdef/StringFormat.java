85f32911dc3eee676e7e08a63eb1425b9a931036
Ivan Korobkov <ivan.korobkov@gmail.com>
2013-05-27 18:11:56 +0400

    java. String format tests, fixed #95.

--- java/src/main/java/io/pdef/StringFormat.java
+++ java/src/main/java/io/pdef/StringFormat.java
@@ -17,10 +17,11 @@ public class StringFormat {
 		this.pdef = checkNotNull(pdef);
 	}
 
-	public Object read(final Class<?> cls, final String s) {
+	@SuppressWarnings("unchecked")
+	public <T> T read(final Class<T> cls, final String s) {
 		checkNotNull(cls);
 		Pdef.TypeInfo info = pdef.get(cls);
-		return read(info, s);
+		return (T) read(info, s);
 	}
 
 	public Object read(final Pdef.TypeInfo info, final String s) {
@@ -32,10 +33,10 @@ public class StringFormat {
 				case INT16: return s == null ? (short) 0 : Short.parseShort(s);
 				case INT32: return s == null ? 0 : Integer.parseInt(s);
 				case INT64: return s == null ? 0 : Long.parseLong(s);
-				case FLOAT: return s == null ? 0.0 : Float.parseFloat(s);
-				case DOUBLE: return s == null ? 0.0 : Double.parseDouble(s);
+				case FLOAT: return s == null ? 0.0f : Float.parseFloat(s);
+				case DOUBLE: return s == null ? 0.0d : Double.parseDouble(s);
 				case STRING: return s;
-				case ENUM: return s == null ? null : info.asEnum().getValues().get(s.toLowerCase());
+				case ENUM: return s == null ? null : info.asEnum().getValues().get(s.toUpperCase());
 			}
 		} catch (Exception e) {
 			throw new FormatException(e);
@@ -51,24 +52,24 @@ public class StringFormat {
 		return write(info, object);
 	}
 
-	public String write(final Pdef.TypeInfo info, final Object object) {
+	public String write(final Pdef.TypeInfo info, final Object o) {
 		checkNotNull(info);
 
 		try {
 			switch (info.getType()) {
-				case BOOL: return ((Boolean) object) ? TRUE : FALSE;
-				case INT16: return Short.toString((Short) object);
-				case INT32: return Integer.toString((Integer) object);
-				case INT64: return Long.toString((Long) object);
-				case FLOAT: return Float.toString((Float) object);
-				case DOUBLE: return Double.toString((Double) object);
-				case STRING: return object == null ? "" : (String) object;
-				case ENUM: return ((Enum<?>) object).name().toLowerCase();
+				case BOOL: return o == null ? FALSE : ((Boolean) o) ? TRUE : FALSE;
+				case INT16: return o == null ? "0" : Short.toString((Short) o);
+				case INT32: return o == null ? "0" : Integer.toString((Integer) o);
+				case INT64: return o == null ? "0" : Long.toString((Long) o);
+				case FLOAT: return o == null ? "0.0" : Float.toString((Float) o);
+				case DOUBLE: return o == null ? "0.0" : Double.toString((Double) o);
+				case STRING: return o == null ? "" : (String) o;
+				case ENUM: return o == null ? "" : ((Enum<?>) o).name().toLowerCase();
 			}
 		} catch (Exception e) {
 			throw new FormatException(e);
 		}
 
-		throw new FormatException("Unsupported type " + object);
+		throw new FormatException("Unsupported type " + o);
 	}
 }

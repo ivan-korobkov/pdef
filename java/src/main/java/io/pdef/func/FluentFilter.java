f62066df5e1928f2975ebdda701efca11dadb163
Ivan Korobkov <ivan.korobkov@gmail.com>
2013-07-16 00:30:44 +0400

    java. Fixed circular calls in FluentFilter toString methods.

--- java/src/main/java/io/pdef/func/FluentFilter.java
+++ java/src/main/java/io/pdef/func/FluentFilter.java
@@ -1,21 +1,15 @@
 package io.pdef.func;
 
 import com.google.common.base.Function;
+import static com.google.common.base.Preconditions.*;
 
 import javax.annotation.Nullable;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 public abstract class FluentFilter<In0, Out0, In1, Out1> implements Filter<In0, Out0, In1, Out1> {
 	public static <In0, Out0, In1, Out1> FluentFilter<In0, Out0, In1, Out1> from(
 			final Filter<In0, Out0, In1, Out1> filter) {
 		checkNotNull(filter);
 		return new FluentFilter<In0, Out0, In1, Out1>() {
-			@Override
-			public String toString() {
-				return filter.toString();
-			}
-
 			@Override
 			public Out0 apply(final In0 input, final Function<In1, Out1> next) {
 				return filter.apply(input, next);
@@ -25,11 +19,6 @@ public abstract class FluentFilter<In0, Out0, In1, Out1> implements Filter<In0, 
 
 	public Function<In0, Out0> then(final Function<In1, Out1> next) {
 		return new Function<In0, Out0>() {
-			@Override
-			public String toString() {
-				return next.toString();
-			}
-
 			@Override
 			public Out0 apply(@Nullable final In0 input) {
 				return FluentFilter.this.apply(input, next);
@@ -41,11 +30,6 @@ public abstract class FluentFilter<In0, Out0, In1, Out1> implements Filter<In0, 
 			final Filter<In1, Out1, In2, Out2> next) {
 		final FluentFilter<In0, Out0, In1, Out1> that = this;
 		return new FluentFilter<In0, Out0, In2, Out2>() {
-			@Override
-			public String toString() {
-				return next.toString();
-			}
-
 			@Override
 			public Out0 apply(final In0 input, final Function<In2, Out2> next1) {
 				return that.apply(input, new Function<In1, Out1>() {

a04d53f5d31262c7cdcdc53870746967a5b92ee8
Ivan Korobkov <ivan.korobkov@gmail.com>
2013-09-23 17:09:25 +0400

    Version 1.0-beta1.

--- compiler/setup.py
+++ compiler/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pdef-compiler',
-    version='1.0-alpha9',
+    version='1.0-beta1',
     url='http://github.com/ivan-korobkov/pdef',
     description='Pdef compiler',
     license='Apache License 2.0',
